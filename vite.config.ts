fbff6042abe7031458e61b1e18ae40a3a4624d17
ankitk26 <myselfankit51@gmail.com>
2026-03-22 02:14:55 +0530

    remove vite plus

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,12 @@ import path from "path";
 import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite-plus";
+import { defineConfig } from "vite";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	fmt: {
-		$schema: "./node_modules/oxfmt/configuration_schema.json",
-		useTabs: true,
-		tabWidth: 4,
-		printWidth: 80,
-		sortImports: {
-			newlinesBetween: false,
-		},
-		sortTailwindcss: {
-			stylesheet: "./src/index.css",
-			functions: ["clsx", "cn"],
-		},
-	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "src"),
